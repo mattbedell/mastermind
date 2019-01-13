7623f3e6d688495c9a64cb310cb2a12d7f9b62f4
Matt Bedell <matt@bedell.co>
2019-01-13 16:13:35 -0500

    add routes

--- src/utils/router.js
+++ src/utils/router.js
@@ -1,6 +1,7 @@
 import VueRouter from 'vue-router';
 import Lobby from '../components/Lobby/index.vue';
 import Game from '../components/Game/index.vue';
+import GameBoard from '../components/GameBoard/index.vue';
 import Join from '../components/Lobby/join.vue';
 
 /* eslint-disable */
@@ -10,6 +11,7 @@ export default new VueRouter({
     { path: '/lobby', component: Lobby },
     { path: '/', component: Game, children: [
       { path: '/', component: Join },
+      { path: '/game', component: GameBoard }
     ] },
   ],
 });
