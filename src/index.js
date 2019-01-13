da4f478083b23aeefc4297856097a7df68cb86a3
Matt Bedell <matt@bedell.co>
2019-01-13 16:14:10 -0500

    add app store, routes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Vuex from 'vuex';
 
 import App from './components/app.vue';
 import router from './utils/router';
 import socket from './plugins/socket';
+import state from './store/index';
 
+Vue.use(Vuex);
 Vue.use(VueRouter);
 Vue.use(socket);
 
+const store = new Vuex.Store(state);
+
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#root');
