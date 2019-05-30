8d38d1822e3a165332148e09aef6bd51a6fd0c7d
Matt Bedell <matt@bedell.co>
2019-05-30 18:21:48 -0400

    use webpack dev server

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 
-const path = require('path');
 const http = require('http');
 const argv = require('argv');
 const express = require('express');
-const history = require('connect-history-api-fallback');
 
 const { port: PORT } = argv.option([{
   name: 'port',
@@ -19,16 +17,5 @@ const Game = require('./src/game/index');
 const app = express();
 const server = http.Server(app);
 const connectedGame = new Game(server);
-
-app.use('/dist', express.static(path.resolve('dist')));
-
-app.use(history({
-  index: '/',
-  verbose: true,
-}));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve('dist/index.html'));
-});
 
 server.listen(PORT, () => console.log(`Listening on localhost:${PORT}`));
