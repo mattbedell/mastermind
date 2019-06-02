41a9c3c74f7217f7088d1dc50bfb773ab83710e1
Matt Bedell <matt@bedell.co>
2019-06-02 16:43:30 -0400

    use separate sock server, app dev server

--- server.js
+++ server.js
@@ -3,19 +3,37 @@ const http = require('http');
 const argv = require('argv');
 const express = require('express');
 
-const { port: PORT } = argv.option([{
+const connectSock = require('./src/connection/index');
+
+const { port: PORT, lobbycodelen: LOBBY_CODE_LENGTH } = argv.option([{
   name: 'port',
   short: 'p',
   type: 'int',
   description: 'Listen on port',
+}, {
+  // TODO: might do something with a load balancer here to adjust lobby code length
+  // dynamically based on current combinations in use, spinning up another server
+  // that requires n+1 character length as n length approaches max combos for that length.
+  // ex: 2 letter codes have 676 combos, spin up a new server with 3 (17,576 combos), etc.
+  name: 'lobbycodelen',
+  short: 'l',
+  type: 'int',
+  description: 'Length of lobby join codes',
 }])
   .run()
   .options;
 
-const Game = require('./src/game/index');
+// const Game = require('./src/game/index');
+
 
 const app = express();
+
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', process.env.APP_HOST);
+//   next();
+// });
+
 const server = http.Server(app);
-const connectedGame = new Game(server);
+connectSock(server, LOBBY_CODE_LENGTH);
 
 server.listen(PORT, () => console.log(`Listening on localhost:${PORT}`));
