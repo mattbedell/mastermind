01c93072d42ef485ba580d37183306852af477cb
Matt Bedell <matt@bedell.co>
2019-06-02 16:49:24 -0400

    add routes

--- src/Components/app.jsx
+++ src/Components/app.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
+import Home from './Home';
+import Join from './Join';
+import Lobby from './Lobby';
+
 export default () => (
   <Router>
     <Route
       exact
       path="/"
-      render={() => (
-        <Link to="/hello">Hello</Link>
-      )}
+      component={Home}
     />
     <Route
       exact
-      path="/hello"
-      render={() => (
-        <Link to="/">Home</Link>
-      )}
+      path="/join"
+      component={Join}
+    />
+    <Route
+      path="/lobby"
+      component={Lobby}
     />
   </Router>
 );
